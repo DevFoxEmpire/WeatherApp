1fcb11cfe4e694cf2fbcc7263d5c808f4e39c5e5
OlliGN <Olga199411@gmail.com>
2022-12-03 13:16:59 -0600

    Initial Commit

---
+++ Weather/Views/Main/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Olha Hladush on 12/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Olha Hladush on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
